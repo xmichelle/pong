00016b3e15d84c45cc31b2cc14ac7285b68cca78
xmichelle <xu.miche@gmail.com>
2017-11-14 15:17:40 -0800

    Created a black canvas. #1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+let canvas
+let canvasContext
 
+window.onload = function() {
+  canvas = document.querySelector('#gameCanvas')
+  canvasContext = canvas.getContext('2d')
+  canvasContext.fillStyle = 'black'
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+}
