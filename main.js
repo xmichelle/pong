152a6b550b503b20cf6e186d6cfaa6a0c509de1d
xmichelle <xu.miche@gmail.com>
2017-11-14 15:49:57 -0800

    Refactored the code. #1

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let canvasContext
 window.onload = function() {
   canvas = document.querySelector('#gameCanvas')
   canvasContext = canvas.getContext('2d')
+  canvasDrawings()
+}
+
+function canvasDrawings() {
   canvasContext.fillStyle = 'black'
   canvasContext.fillRect(0, 0, canvas.width, canvas.height)
 }
