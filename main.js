b2c207583066d46efbdbe11464d441f697147f5f
xmichelle <xu.miche@gmail.com>
2017-11-15 15:59:31 -0800

    Ball can move vertically as well. #3

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let canvas
 let canvasContext
+
 let ballX = 50
 let ballSpeedX = 5
+
+let ballY = 50
+let ballSpeedY = 4
 
 window.onload = function() {
   canvas = document.querySelector('#gameCanvas')
@@ -16,11 +20,19 @@ window.onload = function() {
 
 function moveBall() {
   ballX += ballSpeedX
+  ballY += ballSpeedY
+
   if (ballX <= 0) {
     ballSpeedX = -ballSpeedX
   }
   else if (ballX >= canvas.width) {
     ballSpeedX = -ballSpeedX
+  }
+  else if (ballY <= 0) {
+    ballSpeedY = -ballSpeedY
+  }
+  else if (ballY >= canvas.height) {
+    ballSpeedY = -ballSpeedY
   }
 }
 
@@ -33,7 +45,7 @@ function canvasDrawings() {
   drawRect('white', 0, 210, 10, 100)
 
   // pong ball
-  drawCircle('white', ballX, 100, 10)
+  drawCircle('white', ballX, ballY, 10)
 }
 
 function drawRect(color, xCoordinate, yCoordinate, width, height) {
